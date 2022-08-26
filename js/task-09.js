4a808574cb742f85dca33a46f4a4970fea93dbec
hremenyak <hremenyak@gmail.com>
2022-08-26 18:30:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.body;
+
+const btnRef = document.querySelector('.change-color');
+
+const colorName = document.querySelector('.color');
+
+btnRef.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+
+  colorName.textContent = getRandomHexColor();
+
+}
