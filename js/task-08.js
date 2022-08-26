176e0172eaf2a64e8972aad8d9ac97e45f9c4172
hremenyak <hremenyak@gmail.com>
2022-08-26 17:47:47 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+   
+    let ourData = {};
+
+    ourData.email = email.value;
+    ourData.password = password.value;
+
+
+
+    if (!email.value || !password.value) {
+        window.alert('Please, fill out all the fields!')
+    } else (console.log(ourData)
+       
+    )
+     formRef.reset();
+}
