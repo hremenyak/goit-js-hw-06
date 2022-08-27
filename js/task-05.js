11659fb5e7bb44eb06f5fb8472d50d169b62bbe9
hremenyak <hremenyak@gmail.com>
2022-08-27 12:39:51 +0200

    fix Task 5

--- js/task-05.js
+++ js/task-05.js
@@ -8,10 +8,6 @@ const nameOutputRef = document.querySelector('#name-output');
 nameInputRef.addEventListener('input', onNameInput);
 
 function onNameInput(event) {
-
-      nameOutputRef.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === '') {
-    nameOutputRef.textContent = 'Anonymous'
-  }; 
-    
+  nameOutputRef.textContent = event.currentTarget.value.trim() ? event.currentTarget.value :
+    'Anonymous';
 }
